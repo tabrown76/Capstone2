19e537f9674b15170667aa78bec50ca9055c82d1
Timothy Brown <tabrown76@gmail.com>
2024-05-25 10:23:49 -0400

    deployment prep

--- backend/neweats-seed.sql
+++
@@ -1,8 +0,0 @@
--- user password: "password"
-
-INSERT INTO users (username, password, first_name, last_name, email)
-VALUES ('testuser',
-        '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
-        'Test',
-        'User',
-        'test@gmail.com');
